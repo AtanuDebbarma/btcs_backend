e74fe416369f89fce2f1228196e4833eff51bdcd
Atanu_Mobeet <atanu.fat.mobeet@gmail.com>
2025-05-03 18:00:07 +0530

    alerts comepleted

--- helpers.js
+++ helpers.js
@@ -5,6 +5,7 @@ function logErrorToDatabase({
   errorContext,
   errorDetails,
   serverAttempt = undefined,
+  adminFail = undefined,
 }) {
   const errorRef = adminDb
     .ref(`logs/errors/${controllerName || 'unknown'}`)
@@ -13,9 +14,25 @@ function logErrorToDatabase({
   const serverRef = adminDb
     .ref(`logs/unauthorsizedAttempt/${controllerName || 'unknown'}`)
     .push();
+  const adminRef = adminDb
+    .ref(`logs/adminFail/${controllerName || 'unknown'}`)
+    .push();
 
   if (serverAttempt) {
     return serverRef.set({
+      timestamp: Date.now(),
+      controllerName: controllerName || 'unknown',
+      context: errorContext || 'unknown',
+      error: {
+        message: errorDetails?.message || 'Unknown error',
+        name: errorDetails?.name || null,
+        stack: errorDetails?.stack || null,
+      },
+    });
+  }
+
+  if (adminFail) {
+    return adminRef.set({
       timestamp: Date.now(),
       controllerName: controllerName || 'unknown',
       context: errorContext || 'unknown',
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const {adminAuth} = require('../services/firebaseAdmin');
+const {logErrorToDatabase} = require('../helpers');
 
 async function authenticateFirebaseToken(req, res, next) {
   const authHeader = req.headers.authorization;
@@ -10,9 +11,35 @@ async function authenticateFirebaseToken(req, res, next) {
 
   try {
     const decodedToken = await adminAuth.verifyIdToken(token);
+
+    const adminEmail = process.env.ADMIN_EMAIL;
+    if (decodedToken.email !== adminEmail) {
+      console.log('Access denied: not admin');
+      logErrorToDatabase({
+        controllerName: 'admin',
+        errorContext: 'Unauthorized Admin Attempt',
+        errorDetails: {
+          message: `Unauthorized Admin Attempt`,
+          name: 'Admin Violation',
+        },
+        adminFail: true,
+      });
+      return res.status(403).json({message: 'Access denied: not admin'});
+    }
+
     req.user = decodedToken;
     next();
   } catch (error) {
+    console.log('Token verification failed', error);
+    logErrorToDatabase({
+      controllerName: 'admin',
+      errorContext: 'Unauthorized Admin Attempt',
+      errorDetails: {
+        message: `Unauthorized Admin Attempt ${error}`,
+        name: 'Admin Violation',
+      },
+      adminFail: true,
+    });
     res.status(403).json({message: 'Token verification failed', error});
   }
 }
